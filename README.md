4f87b949b911e648f7b5febf1e1d8ffe85b3e689
Tobias Lasser <lasser@in.tum.de>
2017-06-06 15:54:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Git repository for part 2 of the Praktikum: "Image Reconstruction and Visualization using C++"
+
+## Notes
+How to specifically use this application, additional exernal libraries or code you used etc.
+
+## Team Members:
+ - Someone <someone@somewhere.example>
+ - ...
+
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+
+\
+                      int
+                     _,l;\
+                    char*I,
+                   *O[]={"",
+                  "gjstu","t"
+                 "fdpoe","uij"
+                "se","gpvsui",\
+                  "gjgui","t"
+                 "jyui","tfwf"
+                "oui","fjhiui",
+               "ojoui","ufoui",\
+              "fmfwfoui","uxfmgu"
+             "i","b!qbsusjehf!jo!"
+            "b!qfbs!usff/\xb\xb",""
+               "uxp!uvsumf!epwf"
+              "t-\xb","uisff!gsf"
+             "odi!ifot-!","gpvs!d"
+            "bmmjoh!cjset-!","gjwf"
+           "!hpme!sjoht<\xb","tjy!h"
+          "fftf!b.mbzjoh-!","tfwfo!t"
+         "xbot!b.txjnnjoh-\xb","fjhiu"
+        "!nbjet!b.njmljoh-!","ojof!mbe"
+            "jft!ebodjoh-!","ufo!m"
+           "pset!b.mfbqjoh-\xb","fm"
+          "fwfo!qjqfst!qjqjoh-!","ux"
+         "fmwf!esvnnfst!esvnnjoh-!",""
+        "Po!uif!","!ebz!pg!Disjtunbt!n"
+       "z!usvf!mpwf!hbwf!up!nf\xb","boe"
+      "!"};int putchar(int);int main(void
+     ){while(l<(sizeof O/sizeof*O-2)/2-1){
+          I=O[_=!_?sizeof O/sizeof*O-
+         3:_<(sizeof(O)/sizeof*O-2)/2?
+        sizeof O/sizeof*O-2:_==(sizeof(
+       O)/sizeof*O-2)/2?++l,0:_<(sizeof(
+      O)/sizeof(*O))-3?(_-1)==(sizeof(O)/
+     sizeof*O-2)/2?sizeof O/sizeof*O-1:_-1
+    :_<sizeof(O)/sizeof*O-2?l+1:_<sizeof(O)
+   /sizeof*O-1?l+(sizeof O/sizeof(*O)-2)/2:(
+  sizeof(O)/sizeof*O-2)/2];while(*I){putchar(
+                  *I++-1);}}
+                  return 0;}
+
+//from http://forum.codecall.net/topic/43097-obfuscated-c-contest/?p=388582
