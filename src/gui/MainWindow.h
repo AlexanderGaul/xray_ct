a5b01211021fa0eab8e07bd5ba83e560ef4b8cb8
max_maier2 <Maximilian.Hornung@tum.de>
2017-06-11 15:18:34 +0200

    add first buttons to gui.

--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -1,12 +1,32 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+
 #include <QMainWindow>
+#include <QPushButton>
+#include <QVBoxLayout>
+
 #include "AcquisitionWidget.h"
 
 class MainWindow : public QMainWindow {
 public:
-    MainWindow() : QMainWindow {} {
-        
+    MainWindow() {
+        QVBoxLayout* layout = new QVBoxLayout;
+
+        QHBoxLayout* topLayout = new QHBoxLayout;
+
+        _aButton = new QPushButton("ACQ");
+        _rButton = new QPushButton("REC");
+        _vButton = new QPushButton("VIS");
+
+        topLayout->addWidget(_aButton);
+        topLayout->addWidget(_rButton);
+        topLayout->addWidget(_vButton);
+
+        layout->addItem(topLayout);
+
+        QWidget* rootWidget = new QWidget;
+        rootWidget->setLayout(layout);
+        setCentralWidget(rootWidget);
     }
 public slots:
     
@@ -14,6 +34,9 @@ private slots:
     
 private:
     AcquisitionWidget aWidget;
+    QPushButton* _aButton;
+    QPushButton* _rButton;
+    QPushButton* _vButton;
 };
 
 #endif // MAINWINDOW_H
