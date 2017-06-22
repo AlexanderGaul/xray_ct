3d6da1889cc9a48c4206b32cf8096a58bc3bdffe
Maximilian Hornung <maximilian.hornung@tum.de>
2017-06-22 21:42:49 +0200

    minor change in data container.

--- src/rec/DataContainer.h
+++ src/rec/DataContainer.h
@@ -2,11 +2,14 @@
 
 #include <Eigen/Eigen>
 
+/**
+ * This is the abstract container class representing the system matrix B.
+ * The kind of B is defined in subclasses.
+ * @brief The DataContainer class
+ */
 class DataContainer
 {
 public:
-    DataContainer();
-
     virtual
     void mult(Eigen::Vector3f vector) = 0;
 };
