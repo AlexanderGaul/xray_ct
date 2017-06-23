ae3c5aad139963ad9017fd31ccb52dc36f6daeb2
Maximilian Hornung <maximilian.hornung@tum.de>
2017-06-23 11:39:29 +0200

    changes in playground.

--- test/test_SinogramGeneration.cpp
+++ test/test_SinogramGeneration.cpp
@@ -13,21 +13,21 @@
  */
 TEST_CASE("test sinogram generation")
 {
-    AcquisitionModel model("/home/chips/dev/cpp_practical/part2-team6/volumes/box.edf");
+    // just playground, no real test here
+//    AcquisitionModel model("/home/chips/dev/camprecon/code/build/apps/crossed_sticks/_camp_dci_160.edf");
 
-    // TODO: initialize acquisition pose.
-    int SIZE = 10; // we know the size of the test sample
-    Eigen::VectorXf content(100);
-    for(int i = 0; i<10; i++)
-    {
-        for(int j = 0; j<10; j++)
-        {
-            std::cout << model.forwardProject(i,j) << "\t";
-            content[i*10+j] = model.forwardProject(i,j);
-        }
-        std::cout << std::endl;
-    }
-    Volume volume(Eigen::Vector3f(0,0,0), Eigen::Vector3f(10,10,1), Eigen::Vector3f(1,1,1), content);
+//    int SIZE = 10; // we know the size of the test sample
+//    Eigen::VectorXf content(100);
+//    for(int i = 0; i<10; i++)
+//    {
+//        for(int j = 0; j<10; j++)
+//        {
+//            std::cout << model.forwardProject(i,j) << "\t";
+//            content[i*10+j] = model.forwardProject(i,j);
+//        }
+//        std::cout << std::endl;
+//    }
+//    Volume volume(Eigen::Vector3f(0,0,0), Eigen::Vector3f(10,10,1), Eigen::Vector3f(1,1,1), content);
 
-    EDFHandler::write("OUTPUT", volume);
+//    EDFHandler::write("OUTPUT", volume);
 }
