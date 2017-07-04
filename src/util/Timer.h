96ddb849581bf23dbf2d3f5e90e8208fc4efc367
Maximilian Hornung <maximilian.hornung@tum.de>
2017-07-04 12:45:39 +0200

    possible hotfix for windows build.

--- src/util/Timer.h
+++ src/util/Timer.h
@@ -8,9 +8,10 @@ class Timer
 {
 private:
     typedef std::chrono::high_resolution_clock Clock;
+    typedef std::chrono::time_point<std::chrono::system_clock,std::chrono::system_clock::duration> TimePoint;
 
     std::string _label;
-    std::chrono::system_clock::time_point _begin;
+    TimePoint _begin;
 
 public:
     Timer(std::string label)
@@ -30,7 +31,7 @@ public:
       */
     void end()
     {
-        std::chrono::system_clock::time_point end = Clock::now();
+        TimePoint end = Clock::now();
 
         std::cout << _label << " \t"
                   << std::chrono::duration_cast<std::chrono::milliseconds>(end - _begin).count()
