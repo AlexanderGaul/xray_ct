5a8087a0ad810c67ef65fb15a12c8aef25fe3b73
Alexander Gaul <alexander.gaul@tum.de>
2017-07-11 19:09:34 +0200

    implemented a simple TransferFunction class

---
+++ src/vis/TransferFunction.cpp
@@ -0,0 +1,64 @@
+#include "TransferFunction.h"
+#include <algorithm>
+
+
+TransferFunction::LinearPiece::LinearPiece() :
+    LinearPiece(0, 100, 0, 255, QColor(255, 255, 255))
+{}
+TransferFunction::LinearPiece::LinearPiece(float intensity0, float intensity1, int opacity0, int opacity1, QColor rgb) 
+    :
+    _intensity0(intensity0),
+    _intensity1(intensity1),
+    _opacity0(opacity0),
+    _opacity1(opacity1),
+    _rgb(rgb)
+{}
+
+QColor TransferFunction::LinearPiece::apply(float intensity)
+{
+    if(_intensity0 <= intensity && intensity <= _intensity1)
+    {
+        QColor color {_rgb};
+        int opacity = static_cast<int>(_opacity0 + (_opacity1 - _opacity0) * (intensity - _intensity0) / (_intensity1 - _intensity0));
+        color.setAlpha(std::max(std::min(opacity, 255), 0));
+        return color;
+    }
+}
+
+
+TransferFunction::TransferFunction()
+{
+    _pieces.push_back(LinearPiece());
+}
+TransferFunction::TransferFunction(LinearPiece function)
+{
+    _pieces.push_back(function);
+}
+TransferFunction::TransferFunction(std::vector<LinearPiece> pieces) 
+    :
+    _pieces(pieces)
+{}
+
+QColor TransferFunction::classify(float intensity)
+{
+    QColor color {0, 0, 0, 0};
+    for(auto it =  _pieces.begin(); it != _pieces.end(); it++)
+    {
+        QColor color = it->apply(intensity);
+        if(color.alpha() > 0)
+        {
+            return color;
+        }
+    }
+    return color;
+}
+
+void TransferFunction::addPiece(LinearPiece function)
+{
+    _pieces.push_back(function);
+}
+
+std::vector<TransferFunction::LinearPiece>& TransferFunction::getPieces()
+{
+    return _pieces;
+}
---
+++ src/vis/TransferFunction.h
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#include <QColor>
+
+enum class ColorMixing : bool {
+    Blend,
+    Overlay
+};
+
+class TransferFunction
+{
+public:
+    class LinearPiece
+    {
+    private:
+        float _intensity0;
+        float _intensity1;
+        
+        int _opacity0;
+        int _opacity1;
+        
+        QColor _rgb;
+    
+    public:
+        LinearPiece();
+        LinearPiece(float intensity0, float intensity1, int opacity0, int opacity1, QColor rgb);
+        
+        QColor apply(float intensity);
+    };
+    
+private:
+    std::vector<LinearPiece> _pieces;
+
+public:
+    TransferFunction();
+    TransferFunction(LinearPiece function);
+    TransferFunction(std::vector<LinearPiece> pieces);
+    
+    QColor classify(float intensity);
+    
+    void addPiece(LinearPiece function);
+    std::vector<LinearPiece>& getPieces();
+};
