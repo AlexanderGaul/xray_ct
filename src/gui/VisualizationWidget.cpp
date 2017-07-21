ce7987c7e87c2f91e8dd14153c741c7e415d4453
simon <s.griebel@gmx.de>
2017-07-22 01:55:42 +0200

    added message when the user tries to load a non-existing rec. in the vis.

--- src/gui/VisualizationWidget.cpp
+++ src/gui/VisualizationWidget.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <QGroupBox>
+#include <QMessageBox>
 
 void VisualizationWidget::mprUpdateColorLabel()
 {
@@ -194,6 +195,7 @@ VisualizationWidget::VisualizationWidget() :
 
 void VisualizationWidget::setRec(const std::shared_ptr<const Volume>& vol){
     if(!vol){
+        QMessageBox::warning(this, "No Reconstruction Present!", "You have to generate a reconstruction before it can be visualized!");
         return;
     }
     _visModel.setVolume(*vol);
