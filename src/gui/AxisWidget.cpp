5966e85e01f0e3557cb222699e4147abc0e1ac84
simon <s.griebel@tum.de>
2017-07-23 15:22:40 +0200

    fixed rotation of the AxisWidget

--- src/gui/AxisWidget.cpp
+++ src/gui/AxisWidget.cpp
@@ -3,24 +3,26 @@
 void AxisWidget::paintEvent(QPaintEvent*) {
     const int minSidelength = std::min(this->height(), this->width());
     const float vecLength = minSidelength / 2.f;
-    auto rot = _model.getRotation();
+    //Since the given Rotation is the rotation of the camera, the inverse of the
+    //rotation is the rotation of the object
+    auto rot = _model.getRotation().inverse();
     std::array<Eigen::Vector3f, 3> axesVecs {
-        rot * Eigen::Vector3f::UnitX() * vecLength * 0.8f,
-        rot * Eigen::Vector3f::UnitY() * vecLength * 0.8f, 
-        rot * Eigen::Vector3f::UnitZ() * vecLength * 0.8f};
+        -rot * Eigen::Vector3f::UnitX() * vecLength * 0.8f,
+        -rot * Eigen::Vector3f::UnitY() * vecLength * 0.8f, 
+        -rot * Eigen::Vector3f::UnitZ() * vecLength * 0.8f};
             
     std::array<int, 3> order {0, 1, 2};
         
     //hardcoded bubblesort for three elements
-    if(axesVecs.at(0).z() > axesVecs.at(1).z()){
+    if(axesVecs.at(0).x() > axesVecs.at(1).x()){
         std::swap(axesVecs.at(0), axesVecs.at(1));
         std::swap(order.at(0), order.at(1));
     }
-    if(axesVecs.at(1).z() > axesVecs.at(2).z()){
+    if(axesVecs.at(1).x() > axesVecs.at(2).x()){
         std::swap(axesVecs.at(1), axesVecs.at(2));
         std::swap(order.at(1), order.at(2));
     }
-    if(axesVecs.at(0).z() > axesVecs.at(1).z()){
+    if(axesVecs.at(0).x() > axesVecs.at(1).x()){
         std::swap(axesVecs.at(0), axesVecs.at(1));
         std::swap(order.at(0), order.at(1));
     }
@@ -31,14 +33,14 @@ void AxisWidget::paintEvent(QPaintEvent*) {
     QPainter painter {this};
     painter.translate(this->width()/2, this->height()/2);
     painter.setPen(colors.at(order.at(0)));
-    painter.drawLine(0, 0, axesVecs[0].x(), axesVecs[0].y());
-    painter.drawText(axesVecs[0].x()*1.1, axesVecs[0].y()*1.1, labels.at(order.at(0)));
+    painter.drawLine(0, 0, axesVecs[0].y(), axesVecs[0].z());
+    painter.drawText(axesVecs[0].y()*1.1, axesVecs[0].z()*1.1, labels.at(order.at(0)));
         
     painter.setPen(colors.at(order.at(1)));
-    painter.drawLine(0, 0, axesVecs[1].x(), axesVecs[1].y());
-    painter.drawText(axesVecs[1].x()*1.1, axesVecs[1].y()*1.1, labels.at(order.at(1)));
+    painter.drawLine(0, 0, axesVecs[1].y(), axesVecs[1].z());
+    painter.drawText(axesVecs[1].y()*1.1, axesVecs[1].z()*1.1, labels.at(order.at(1)));
         
     painter.setPen(colors.at(order.at(2)));
-    painter.drawLine(0, 0, axesVecs[2].x(), axesVecs[2].y());
-    painter.drawText(axesVecs[2].x()*1.1, axesVecs[2].y()*1.1, labels.at(order.at(2)));
+    painter.drawLine(0, 0, axesVecs[2].y(), axesVecs[2].z());
+    painter.drawText(axesVecs[2].y()*1.1, axesVecs[2].z()*1.1, labels.at(order.at(2)));
 }
